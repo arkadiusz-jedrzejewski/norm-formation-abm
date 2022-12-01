de9db6fb73b4f22124dccd76c3902f857318f0ed
Arkadiusz <arekjd@gmail.com>
2022-12-01 13:27:42 +0100

    if statement removed

--- norm_formation_abm/norm_formation_abm.cpp
+++ norm_formation_abm/norm_formation_abm.cpp
@@ -14,31 +14,28 @@
 
 int main(int argc, char** argv)
 {
-    if (argc > 1)
-    {
-        int seed = atoi(argv[1]);
-        std::mt19937_64 generator(seed);
-
-        double p = atof(argv[2]);
-        BernoulliDistribution distribution(generator, p);
-
-        double q = atof(argv[3]);
-        Power conformity_function(q);
-
-        double f = atof(argv[4]);
-        VoterIndependence nonconformity_function(f);
-
-        int system_size = atoi(argv[5]);
-        int init_opinions = atoi(argv[6]);
-        SocialSystem social_system(system_size,
-            init_opinions,
-            distribution,
-            conformity_function,
-            nonconformity_function,
-            generator);
-
-        int time_horizon = atoi(argv[7]);
-        std::string file_name = argv[8];
-        social_system.simulation(time_horizon, file_name);
-    }
+    int seed = atoi(argv[1]);
+    std::mt19937_64 generator(seed);
+    
+    double p = atof(argv[2]);
+    BernoulliDistribution distribution(generator, p);
+    
+    double q = atof(argv[3]);
+    Power conformity_function(q);
+    
+    double f = atof(argv[4]);
+    VoterIndependence nonconformity_function(f);
+    
+    int system_size = atoi(argv[5]);
+    int init_opinions = atoi(argv[6]);
+    SocialSystem social_system(system_size,
+        init_opinions,
+        distribution,
+        conformity_function,
+        nonconformity_function,
+        generator);
+    
+    int time_horizon = atoi(argv[7]);
+    std::string file_name = argv[8];
+    social_system.simulation(time_horizon, file_name);
 }
