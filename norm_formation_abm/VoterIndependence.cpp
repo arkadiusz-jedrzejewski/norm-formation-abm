cbbfff5e77ee24529b27a3ecfff5936577d6d7ea
Arkadiusz <arekjd@gmail.com>
2022-12-09 16:22:57 +0100

    removed unused variable in VoterIndependence

--- norm_formation_abm/VoterIndependence.cpp
+++ norm_formation_abm/VoterIndependence.cpp
@@ -8,7 +8,6 @@ VoterIndependence::VoterIndependence(double f)
 void VoterIndependence::run(Agent& agent, double conc, RNG& generator)
 {
 	std::uniform_real_distribution<double> unif_real_distribution(0, 1);
-	int opinion = agent.get_opinion();
 	double rand = unif_real_distribution(generator);
 
 	if (rand < f)
